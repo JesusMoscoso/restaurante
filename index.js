fc596a59dbc28d7ee9d3ec9e767fb28b490bde91
JesusMoscoso <MoscosoJesus481@gamil.com>
2026-03-03 09:30:00 -0600

    se hizo mejoras en el navbar y se incorporaron nuevas pages

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
- let lang = "es";
+const images = [
+  "imagenes/1.jpg",
+  "imagenes/2.jpg",
+  "imagenes/3.jpg",
+  "imagenes/4.jpg",
+  "imagenes/5.jpg",
+  "imagenes/6.jpg",
+  "imagenes/7.jpg",
+  "imagenes/8.jpg",
+  "imagenes/9.jpg",
+  "imagenes/10.jpg",
+  "imagenes/11.jpg",
+  "imagenes/12.jpg",
+  "imagenes/13.jpg",
+  "imagenes/14.jpg",
+  "imagenes/15.jpg",
+  "imagenes/16.jpg",
+  "imagenes/17.jpg",
+  "imagenes/18.jpg",
+  "imagenes/19.jpg",
+  "imagenes/20.jpg",
+  "imagenes/21.jpg",
+  "imagenes/22.jpg",
+];
 
-      const translations = {
-        es: {
-          brand: "Bosque Nuboso",
-          tours: "Tours",
-          restaurant: "Restaurante",
-          viewpoints: "Miradores",
-          souvenirs: "Souvenirs",
-          reserve: "Reservar",
-          title: "Bosque Nuboso<br>La Paz",
-          subtitle: "Naturaleza viva en San Ramón, Costa Rica",
-          explore: "Explorar Tours",
-          how: "Cómo llegar",
-          infoTitle: "Un viaje al corazón del bosque nuboso",
-          infoText:
-            "Explora senderos entre la niebla y descubre la riqueza natural y majestuosa del bosque nuboso.",
-          map: "Ver en mapa",
-          location: "📍 San Ramón, Alajuela, Costa Rica",
-          copyright: "© 2025 Bosque Nuboso La Paz",
-        },
-        en: {
-          brand: "Cloud Forest",
-          tours: "Tours",
-          restaurant: "Restaurant",
-          viewpoints: "Viewpoints",
-          souvenirs: "Souvenirs",
-          reserve: "Book Now",
-          title: "Cloud Forest<br>La Paz",
-          subtitle: "Living nature in San Ramón, Costa Rica",
-          explore: "Explore Tours",
-          how: "How to get there",
-          infoTitle: "A journey to the heart of the cloud forest",
-          infoText:
-            "Explore misty trails and discover the natural richness and beauty of the cloud forest.",
-          map: "View on map",
-          location: "📍 San Ramón, Alajuela, Costa Rica",
-          copyright: "© 2025 La Paz Cloud Forest",
-        },
-      };
+const carousel = document.getElementById("carouselContent");
 
-      document.getElementById("btnLang").addEventListener("click", () => {
-        lang = lang === "es" ? "en" : "es";
-        document.getElementById("btnLang").textContent =
-          lang === "es" ? "EN" : "ES";
+images.forEach((img, index) => {
+  const div = document.createElement("div");
+  div.classList.add("carousel-item", "hero-slide");
 
-        document.querySelectorAll("[data-key]").forEach((el) => {
-          el.innerHTML = translations[lang][el.dataset.key];
-        });
-      });
+  if (index === 0) {
+    div.classList.add("active"); // primera imagen activa
+  }
+
+  div.style.backgroundImage = `url("${img}")`;
+  carousel.appendChild(div);
+});
